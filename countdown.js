afad5b470d679e1047320e6a763abfb8fa21e088
zeynep çırçır <81877446+zeynepcircir@users.noreply.github.com>
2022-09-02 11:27:59 +0300

    I fixed the erors in using interval.

--- countdown.js
+++ countdown.js
@@ -21,23 +21,57 @@ selectedSec.addEventListener("change", () => {      //saniye 10dan küçük oldu
   selectedSec.value < 10 ? "0" + selectedSec.value : selectedSec.value;
 });
 
+// //UI LOGIC
+
+// //resetUI,updateUI,stopUI,startUI
+
+// function startUI(min,sec) {
+//   //1-kutu ici deger degis
+//   //kutu ici renk degis
+
+// }
+
+// function updateMinAndSec(min,sec){
+//   const min=document.querySelector('min')
+//   const sec=document.querySelector('sec')
+//   min.textContent
+//   sec.textContent
+// }
+
+// //APPLICATION LOGIC
+
+// function startApp(params) {
+//   //kutudan degerleri al, number a donustur
+//   //interval fonksiyonu cagir, application logic guncellesin
+//   //interval icerisinde ayri bir fonksiyon
+
+// }
+
 startButton.addEventListener("click", startTimer);
 
 resetButton.addEventListener("click", () => {
-  stop = true;
+   stop = true;
   minute.textContent = "00";
   second.textContent = "00";
   selectedMin.value = "00";
   selectedSec.value = "00";
-  stop = false;
+  clearInterval(interval);
+  });
+
+
+
+stopButton.addEventListener("click", () => {
+  stop = true;
+  clearInterval(interval);
 });
 
 
 
-function startTimer() {       
+function startTimer() {   
+  stop = false;    
   let min = minute.textContent;
   let sec = second.textContent;
-
+  
   const interval = setInterval(() => {    //girdiğimiz değerlere göre sürenin akış algorisması
     sec--;
     sec = sec < 10 ? "0" + sec : sec;
@@ -46,17 +80,49 @@ function startTimer() {
       sec = 59;
     }
     if ((min == 00 && sec == 00) || (min == 0 && sec == 0)) {
-      clearInterval(interval);
       window.alert("Time is up!..");
       selectedMin.value = "00";
-      secilenSec.value = "00";
+      selectedSec.value = "00";
+      clearInterval(interval);
     }
     if (stop) {
       clearInterval(interval);
       return;
     }
-
+    
     minute.textContent = min;
     second.textContent = sec;
+    
   }, 1000);   //saniyede bir değişim oluyor.
+  
 }
+
+
+function start() {
+  document.getElementById("board").style.backgroundColor = "rgb(191, 162, 219)";
+}
+
+function stops() {
+  document.getElementById("board").style.backgroundColor = "rgb(255, 113, 113)";
+}
+
+function reset() {
+  document.getElementById("board").style.backgroundColor = "rgb(159, 216, 223)";
+}
+
+
+// const INFO = (function () {           
+//   let minute = 0;
+//   let second = 0;
+
+//   function setMinute(val) {
+//     minute = minute + val;
+//   }
+//   function setSecond() {
+//     second = second + val;
+//   }
+//   return {
+//     setMinute,
+//     setSecond,
+//   };
+// })();
